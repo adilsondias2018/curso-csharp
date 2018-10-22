cdda7f0827277ad31ed6eb13623167c663843a8d
adilsondias2018 <adilsondias@outlook.com>
2018-10-22 16:02:08 -0300

    Acresentando a classe PosicaoXadrez juntamente o método toPosicao

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -9,12 +9,13 @@ namespace XadrezConsole
     {
         static void Main(string[] args)
         {
+            /*
             try
             {
                 Tabuleiro tab = new Tabuleiro(8, 8);
 
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 9));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
                 tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
                 Tela.imprimirTabuleiro(tab);
@@ -27,6 +28,12 @@ namespace XadrezConsole
                 Console.WriteLine(e.Message);
                 Console.ReadLine();
             }
+            */
+
+            PosicaoXadrez pos = new PosicaoXadrez('d', 2);
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.toPosicao());
+            Console.ReadLine();
 
 
         }
